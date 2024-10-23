2747241981a2479f3a8c096bab5b8b6aa55f761c
malbae <akmal67e@gmail.com>
2024-10-23 13:02:17 +0700

    Files Upload

---
+++ 06_Interaksi_Pengguna/TP/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Latihan BottomNavigationBar',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: BottomNavBarExample(),
+    );
+  }
+}
+
+class BottomNavBarExample extends StatefulWidget {
+  @override
+  _BottomNavBarExampleState createState() => _BottomNavBarExampleState();
+}
+
+class _BottomNavBarExampleState extends State<BottomNavBarExample> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Center(
+        child: Text('Ini adalah halaman Home', style: TextStyle(fontSize: 20))),
+    Center(
+        child:
+            Text('Ini adalah halaman Wisata', style: TextStyle(fontSize: 20))),
+    Center(
+        child:
+            Text('Ini adalah halaman Profil', style: TextStyle(fontSize: 20))),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Latihan BottomNavigationBar'),
+      ),
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.place),
+            label: 'Wisata',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
