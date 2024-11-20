745773d2a656f6a8249497f959423039165cf1a1
malbae <136548472+malbae@users.noreply.github.com>
2024-11-20 10:17:09 +0700

    Update main.dart

--- 10_Data_Storage_Bagian_1/GUIDED/lib/main.dart
+++ 10_Data_Storage_Bagian_1/GUIDED/lib/main.dart
@@ -1,27 +1,21 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2761
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+import 'package:flutter/material.dart';
 
-\f0\fs24 \cf0 import 'package:flutter/material.dart';\
-import 'view/my_db_view.dart';\
-\
-void main() \{\
-  runApp(const MyApp());\
-\}\
-\
-class MyApp extends StatelessWidget \{\
-  const MyApp(\{Key? key\}) : super(key: key);\
-\
-  @override\
-  Widget build(BuildContext context) \{\
-    return MaterialApp(\
-      debugShowCheckedModeBanner: false,\
-      title: 'Praktikum Database - Sqflite',\
-      theme: ThemeData(primarySwatch: Colors.blue),\
-      home: const MyDbView(),\
-    );\
-  \}\
-\}}
+import 'view/my_db_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Praktikum Database - Sqflite',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyDbView(),
+    );
+  }
+}
